1ac8290f51621d0c1e41fb58c8e4fb3cb8a59242
Juntaroo <juliangodoy.epet20@gmail.com>
2024-06-20 21:14:13 -0300

    continuando con proyecto

--- public/index.js
+++ public/index.js
@@ -1,25 +1,33 @@
 const socket = io();
 
 //Obtengo los elementos de el romulario
-var form = document.getElementById('form');
-var input = document.getElementById('input');
-var mensajes = document.getElementById('mensajes')
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const usuario = document.getElementById('usuario');
+const mensajes = document.getElementById('mensajes')
+
 
 //Si la persona clickea enviar, verifica si tiene contenido el input y lo envia al servidor
 //Despues, vuelve a poner el valor en blanco
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-    if (input.value) {
-        socket.emit('chat message', input.value);
+    if (usuario.value && input.value) {
+        const message = {
+            user: usuario.value,
+            text: input.value
+        };
+        socket.emit('chat message', message);
         input.value = '';
+    } else{
+        alert("Debe completar los campos para enviar los mensajes")
     }
-})
+});
 
 //Envia el mensaje en forma de li en HTML
 //Va agregando los mensajes y pone un window,scroll para que el usuario no necesite scrollear para ver el último mensaje
 socket.on('chat message', function(msg) {
     var text = document.createElement('li');
-    text.textContent = msg;
+    text.textContent = `${msg.user}: ${msg.text}`;
     mensajes.appendChild(text);
     window.scrollTo(0, document.body.scrollHeight);
 });
